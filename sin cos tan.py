4580ed0e977f718b89c22e185c7cd1cb3c7e0599
TuantheTuna <122241876+TuantheTuna@users.noreply.github.com>
2023-01-12 13:22:19 +0700

    Update sin cos tan.py

--- sin cos tan.py
+++ sin cos tan.py
@@ -1,14 +1,20 @@
-import math
-trig = input('do you want to calculate sin, cos, tan?')
-if trig == 'cos':
-        a= eval(input('what is the angle measure?'))
-        result = math.cos(math.radians(a))
-        print('the answer is'+ str(round(result, 3)))
-elif trig == 'tan':
-        a= eval(input('what is the angle measure?'))
-        result = math.tan(math.radians(a))
-        print('the answer is'+ str(round(result, 3)))
-elif trig == 'sin':
+print('1. SIN')
+print('2. COS')
+print('3. TAN')
+
+operation = input('Select an operations to perform: ')
+if operation == '1':
         a= eval(input('what is the angle measure?'))
         result = math.sin(math.radians(a))
         print('the answer is'+ str(round(result, 3)))
+elif operation == '2':
+        a= eval(input('what is the angle measure?'))
+        result = math.cos(math.radians(a))
+        print('the answer is'+ str(round(result, 3)))
+elif operation == '3':
+        a= eval(input('what is the angle measure?'))
+        result = math.tan(math.radians(a))
+        print('the answer is'+ str(round(result, 3)))
+
+else:
+    print('Other operations:')
